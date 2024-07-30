873f52006a4da430e7199b36a6a865242a2fa7b6
derya <derya00burhanoglu@gmail.com>
2024-07-30 15:29:34 +0300

    part1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>İş İlan Portalı</h1>
+      <div className="header">
+        <div className="arama_kutusu">
+             <input type="text" placeholder="İş İlanı Arayın"/>
+             <button id="arama_butonu">Ara</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
